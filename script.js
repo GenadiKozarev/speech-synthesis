ee56bb899f628bbc17b845d2b92a9a3d7b51ad14
gkoz <genadi.kozarev+github@gmail.com>
2024-11-27 16:48:45 +0000

    restarts speech upon new voice being selected

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const msg = new SpeechSynthesisUtterance();
 msg.text = document.querySelector('[name="text"]').value;
 let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.getElementById('speak');
+const stopButton = document.getElementById('stop');
 
 function populateVoices() {
     voices = speechSynthesis.getVoices();
@@ -15,6 +18,15 @@ function populateVoices() {
 
 const setVoice = e => {
     msg.voice = voices.find(voice => voice.name === e.target.value);
+    restartSpeech();
+};
+
+const restartSpeech = ({ startOver = true } = {}) => {
+    speechSynthesis.cancel();
+    console.log(startOver);
+    if (startOver) {
+        speechSynthesis.speak(msg);
+    }
 };
 
 populateVoices();
